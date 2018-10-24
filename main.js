7f092566f59d4c91bc57138c0f5143d2b3e7e098
Buba Korowski <Jakub.B.Borowski@gmail.com>
2018-10-24 11:29:26 +0200

    go to top button in JS

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//  BURGER MENU
 const burger = document.querySelector(".burger");
 const icoBurger = document.querySelector(".fa-bars");
 const icoX = document.querySelector(".fa-times");
@@ -8,6 +9,28 @@ burger.addEventListener("click", () => {
   icoBurger.classList.toggle("show");
   aside.classList.toggle("show");
   icoX.classList.toggle("show");
+  icoArrow.classList.toggle("show");
 
+  Arrow.classList.toggle("active");
   burger.classList.toggle("active");
 });
+
+//  GO TO TOP BUTTON
+const heightFromTop = document.querySelector(".about").offsetTop;
+window.onscroll = () => {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop >= heightFromTop ||
+    document.documentElement.scrollTop >= heightFromTop
+  ) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+function goTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
